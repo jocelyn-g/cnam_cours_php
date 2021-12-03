e7181de1500840547542a9bd2801c2257890bee2
Jocelyn GEZ <jocelyn.gez@gmail.com>
2021-12-03 15:47:48 +0100

    Création d'un nouveau projet
    - ajout d'une page d'accueil
    - ajout d'un hello world en php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'accueil</title>
+</head>
+<body>
+
+    <h1>Page d'acceuil</h1>
+
+    <?php
+
+    echo 'Du PHP dans le HTML <br>';
+    echo '(En réalité il ne faut surtout pas le faire.)';
+
+    ?>
+    
+</body>
+</html>
