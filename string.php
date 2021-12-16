992573654b858b3740c796025b8ed3855e1e3c33
Jocelyn GEZ <jocelyn.gez@gmail.com>
2021-12-16 12:40:05 +0100

    Ajout de code
    - création d'un code sur les chaines de caractère

---
+++ string.php
@@ -0,0 +1,16 @@
+<?php
+
+$text1 = "foo";
+$text2 = $text1 . " bar";
+
+echo $text1 . "<br>";
+echo $text2 . "<br>";
+
+$temperature = 10.5;
+$report = "La température est de $temperature °C";
+
+echo $report . "<br>\n"
+
+
+
+?>
