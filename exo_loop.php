49c008b1df3503b114a27b47f039ff712ccededd
Jocelyn GEZ <jocelyn.gez@gmail.com>
2022-01-13 11:57:53 +0100

    Modification 1 du cours loop et exo 2
    - nouvelle modification tab dans boucle
    - Ecrivez une boucle for qui compte les nombres de 3 en trois de 0 à 100

--- exo_loop.php
+++ exo_loop.php
@@ -4,6 +4,14 @@
 Ecrivez une noucle for qui compte à rebours de 10 à 0.
 */
 
-for ($i = 10 ; $i >= 0 ; $i--){
+//for ($i = 10 ; $i >= 0 ; $i--){
+//    echo "$i<br>\n";
+//}
+
+/*
+Ecrivez une boucle for qui compte les nombres de 3 en trois de 0 à 100.
+*/
+
+for ($i = 0 ; $i <= 100 ; $i += 3){
     echo "$i<br>\n";
 }
--- loop.php
+++ loop.php
@@ -3,3 +3,9 @@
 for ($i = 0 ; $i < 10 ; $i++) {
     echo "$i<br>\n";
 }
+
+$fruits = ['Ananas', 'Banane', 'Cerise', 'Orange'];
+
+for ($i = 0 ; $i < count($fruits) ; $i++) {
+    echo $fruits[$i] . "<br>\n";
+}
